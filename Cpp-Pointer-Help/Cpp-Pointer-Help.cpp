1dfaf2f8a6f02c16a6570710221128e8f14359d7
PotatoSauceVFX <rj@potatosaucevfx.com>
2017-06-04 00:13:24 -0400

    Added some clarification to the comments

--- Cpp-Pointer-Help/Cpp-Pointer-Help.cpp
+++ Cpp-Pointer-Help/Cpp-Pointer-Help.cpp
@@ -8,24 +8,24 @@ Author: PotatoSauceVFX | 2017
 
 using namespace std;
 
-// Declare dad's room to room 100
+// Declare integer for dad's room to be room 100
 int dadsRoom = 100;
 
-// Set pointer "mySon" to point to dad's room mem location
+// Set pointer "mySon" to point to dad's room mem location. (The dad's son was told what room his dad is in)
 int* mySon = &dadsRoom;
 
 int main() {
 
-	// Prints the address
+	// Prints the address of dad's room variable
 	cout << "Address is: " << &dadsRoom << endl;
 
-	// Prints out the address of dad's room
+	// Prints out the address of dad's room from mySon pointer
 	cout << "Variable mySon is: " << mySon << endl;
 
-	// Prints out 100 (Value located at mem address)
+	// Prints out 100 (Value located at mem address for dad's room)
 	cout << "Variable *mySon is: " << *mySon << endl;
 
-
+	// This is added to keep the program from terminating right away
 	cin.ignore(10, '\n');
 	cin.get();
 	return 0;
