d8bbd4fb6b448299d28ac5df5811df1a1f72e163
PotatoSauceVFX <rj@potatosaucevfx.com>
2017-06-04 00:03:23 -0400

    Initial Commit

--- Cpp-Pointer-Help/Cpp-Pointer-Help.cpp
+++ Cpp-Pointer-Help/Cpp-Pointer-Help.cpp
@@ -1,12 +1,20 @@
-// Cpp-Pointer-Help.cpp : Defines the entry point for the console application.
-//
+/*
+Cpp-Pointer-Help.cpp
+Author: PotatoSauceVFX | 2017
+*/
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
 
+	cout << "Hello, world!" << endl;
+
+	cin.ignore(10, '\n');
+	cin.get();
     return 0;
 }
 
