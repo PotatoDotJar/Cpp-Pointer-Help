130e950cfb5eaa3db56d4d3e2e59e1fa593a217c
PotatoSauceVFX <rj@potatosaucevfx.com>
2017-06-04 00:06:35 -0400

    Added hotel room scenario for a better understanding of pointers.

--- Cpp-Pointer-Help/Cpp-Pointer-Help.cpp
+++ Cpp-Pointer-Help/Cpp-Pointer-Help.cpp
@@ -8,13 +8,26 @@ Author: PotatoSauceVFX | 2017
 
 using namespace std;
 
-int main()
-{
+// Declare dad's room to room 100
+int dadsRoom = 100;
 
-	cout << "Hello, world!" << endl;
+// Set pointer "mySon" to point to dad's room mem location
+int* mySon = &dadsRoom;
+
+int main() {
+
+	// Prints the address
+	cout << "Address is: " << &dadsRoom << endl;
+
+	// Prints out the address of dad's room
+	cout << "Variable mySon is: " << mySon << endl;
+
+	// Prints out 100 (Value located at mem address)
+	cout << "Variable *mySon is: " << *mySon << endl;
+
 
 	cin.ignore(10, '\n');
 	cin.get();
-    return 0;
+	return 0;
 }
 
